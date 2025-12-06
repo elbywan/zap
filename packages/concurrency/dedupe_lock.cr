f79d99f65bfdfed7175468871808c65cd10ae822
Julien Elbaz <elbywan@hotmail.com>
2025-12-06 10:23:51 +0100

    fix: add retry limit to DedupeLock to prevent unbounded recursion
    If the producer block keeps failing, consumers would retry indefinitely
    via recursive calls, potentially causing stack overflow. Added a
    configurable max_retries parameter (default 3) to prevent this.

--- packages/concurrency/dedupe_lock.cr
+++ packages/concurrency/dedupe_lock.cr
@@ -2,17 +2,26 @@ require "./data_structures/safe_hash"
 require "./mutex"
 
 module Concurrency::DedupeLock(T)
+  # Maximum number of retries when the producer fails and closes the channel
+  DEDUPE_MAX_RETRIES = 3
+
   @lock = Concurrency::Mutex.new(:unchecked)
   @channels = {} of String => Channel(T)
 
-  def dedupe(key : String, &block : -> T) : T
+  def dedupe(key : String, max_retries : Int32 = DEDUPE_MAX_RETRIES, &block : -> T) : T
+    dedupe_impl(key, max_retries, &block)
+  end
+
+  private def dedupe_impl(key : String, retries_remaining : Int32, &block : -> T) : T
     @lock.lock
     if chan = @channels[key]?
       @lock.unlock
       begin
         value = chan.receive
       rescue Channel::ClosedError
-        return dedupe(key, &block)
+        # Producer failed; retry if we have retries left
+        raise "DedupeLock: max retries exceeded for key '#{key}'" if retries_remaining <= 0
+        return dedupe_impl(key, retries_remaining - 1, &block)
       end
     else
       @channels[key] = Channel(T).new
@@ -54,14 +63,22 @@ module Concurrency::DedupeLock::Global
     @@%lock = Concurrency::Mutex.new(:unchecked)
     @@%channels = Hash(String, Channel({{type}})).new
 
-    def self.dedupe_{{name}}(key : String, &block : -> {{type}}) : {{type}}
+    DEDUPE_MAX_RETRIES_{{name.upcase}} = 3
+
+    def self.dedupe_{{name}}(key : String, max_retries : Int32 = DEDUPE_MAX_RETRIES_{{name.upcase}}, &block : -> {{type}}) : {{type}}
+      dedupe_{{name}}_impl(key, max_retries, &block)
+    end
+
+    private def self.dedupe_{{name}}_impl(key : String, retries_remaining : Int32, &block : -> {{type}}) : {{type}}
       @@%lock.lock
       if chan = @@%channels[key]?
         @@%lock.unlock
         begin
           value = chan.receive
         rescue Channel::ClosedError
-          return dedupe_{{name}}(key, &block)
+          # Producer failed; retry if we have retries left
+          raise "DedupeLock: max retries exceeded for key '#{key}'" if retries_remaining <= 0
+          return dedupe_{{name}}_impl(key, retries_remaining - 1, &block)
         end
       else
         @@%channels[key] = Channel({{type}}).new
