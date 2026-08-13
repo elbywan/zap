4810c11181b0f4458afb0151f341537ff6b85aa8
Julien Elbaz <elbywan@hotmail.com>
2026-08-13 13:09:24 +0200

    spec: cover the scoped named-registry form

--- packages/commands/spec/install/integration/named_registries._spec.cr
+++ packages/commands/spec/install/integration/named_registries._spec.cr
@@ -54,6 +54,29 @@ describe "named registries", tags: "integration" do
     end
   end
 
+  it "resolves a scoped package through the named registry alias" do
+    default = Zap::Integration::Registry.new
+    named = Zap::Integration::Registry.new
+    tmpdir = Path.new(Dir.tempdir, "zap-it-#{Random::Secure.hex(4)}")
+    begin
+      named.add("@corp/lib", "1.0.0", It.pkg("@corp/lib", "1.0.0"), {"index.js" => "from-named"})
+
+      Dir.mkdir_p(tmpdir)
+      File.write(tmpdir / "package.json", %({"name":"app","version":"1.0.0","dependencies":{"@corp/lib":"work:^1.0.0"},"zap":{"named_registries":{"work":"#{named.base_url}"}}}))
+      File.write(tmpdir / ".npmrc", "registry=#{default.base_url}/\n")
+      config = Core::Config.new.copy_with(prefix: tmpdir.to_s, store_path: (tmpdir / "store").to_s, silent: true)
+      ic = Commands::Install::Config.new.copy_with(workers: 1, frozen_lockfile: false, save: true)
+      Commands::Install.run(config, ic, raise_on_failure: true, reporter: Reporter::Null.new)
+
+      File.read(tmpdir / "node_modules/@corp/lib/index.js").should eq("from-named")
+      File.read(tmpdir / "zap.lock").should contain("@corp/lib@work:1.0.0")
+    ensure
+      default.stop
+      named.stop
+      FileUtils.rm_rf(tmpdir)
+    end
+  end
+
   it "keeps the named registry alias when updating the range" do
     default = Zap::Integration::Registry.new
     named = Zap::Integration::Registry.new
